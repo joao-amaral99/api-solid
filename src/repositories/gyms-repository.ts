93b6e876f84fb0339a460ec27c7dabcd30043a1b
João Amaral <joao.amaral.almeida99@gmail.com>
2025-07-15 17:50:38 -0300

    feat: implement additional methods in GymsRepository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,16 @@
-import { Gym } from 'generated/prisma'
+import { Gym, Prisma } from 'generated/prisma'
+
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
+
+  searchMany(query: string, page: number): Promise<Gym[]>
+
+  create(data: Prisma.GymCreateInput): Promise<Gym>
 }
