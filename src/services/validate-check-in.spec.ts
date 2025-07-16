1481bb35ff60674343405f77c4a89d61d89657a2
João Amaral <joao.amaral.almeida99@gmail.com>
2025-07-16 10:53:03 -0300

    test: create validate check-in tests

---
+++ src/services/validate-check-in.spec.ts
@@ -0,0 +1,62 @@
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ValidateCheckInService } from './validate-check-in'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+
+let checkInRepository: InMemoryCheckInsRepository
+let validateCheckInService: ValidateCheckInService
+
+describe('Validate Check-in Service', () => {
+  beforeEach(async () => {
+    checkInRepository = new InMemoryCheckInsRepository()
+    validateCheckInService = new ValidateCheckInService(checkInRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to validate chec-in', async () => {
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const { checkIn } = await validateCheckInService.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to validate an inexistent check-in', async () => {
+    await expect(() =>
+      validateCheckInService.execute({
+        checkInId: 'inexistent-check-in-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2025, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const TWENTY_ONE_MINUTES_IN_MS = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(TWENTY_ONE_MINUTES_IN_MS)
+
+    await expect(() =>
+      validateCheckInService.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
+  })
+})
