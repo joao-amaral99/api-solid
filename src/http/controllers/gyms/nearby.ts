4ecc1d0361da8cb8dffbecb2b3673c4b2bdfa498
João Amaral <joao.amaral.almeida99@gmail.com>
2025-07-22 17:56:35 -0300

    feat: create gyms nearby controller

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsService } from '@/services/factories/make-fetch-nearby-gyms-service'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.body)
+
+  const fetchNearbyGymService = makeFetchNearbyGymsService()
+
+  const { gyms } = await fetchNearbyGymService.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
