46b571f12069d77340b101fbeaec169f53b05e85
João Amaral <joao.amaral.almeida99@gmail.com>
2025-07-23 15:15:40 -0300

    test: create register gyms e2e tests

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym Controller e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Gym 01',
+        phone: '1198885555',
+        description: 'Best Gym Ever',
+        latitude: -14.8897792,
+        longitude: -40.8485888,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
