82aa5227cf12db0aa2bf5649ae5389f985a881f4
João Amaral <joao.amaral.almeida99@gmail.com>
2025-07-23 15:15:58 -0300

    test: create nearby gym e2e tests

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Nearby Gyms Controller e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get list gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near Gym',
+        phone: '1198885555',
+        description: 'Near Gym Ever',
+        latitude: -14.8897792,
+        longitude: -40.8485888,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far Gym',
+        phone: '1198885555',
+        description: 'Far Gym Ever',
+        latitude: -14.6828452,
+        longitude: -40.5058711,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -14.8857976,
+        longitude: -40.8533953,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: 'Near Gym' }),
+    ])
+  })
+})
