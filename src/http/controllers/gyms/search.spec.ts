9c3a818997e30b841c01aabc3c703aa994e25b08
João Amaral <joao.amaral.almeida99@gmail.com>
2025-07-23 15:16:14 -0300

    test: create search gyms e2e tests

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,43 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gyms Controller e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Gym 01',
+        phone: '1198885555',
+        description: 'Best Gym Ever',
+        latitude: -14.8897792,
+        longitude: -40.8485888,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'Gym 01',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: 'Gym 01' }),
+    ])
+  })
+})
